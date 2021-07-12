293701fb47b130549a0ffbeb36dcc0456242b200
Mary Beth Ingram <mbingram@clmbr-mbingram.local>
2021-07-12 16:09:42 -0600

    og push

---
+++ src/hotspotsData.js
@@ -0,0 +1,9 @@
+export const classDuration = 30;
+export const timelineArray = [
+    { id: 'warmup-1', hotspot: 'Warmup', tempo: '40', reach: 15, resistance: 4, duration: 30 },
+    { id: 'warmup-2', hotspot: 'Warmup', tempo: '60', reach: 15, resistance: 4, duration: 30 },
+    { id: 'stretch-1', hotspot: 'Stretch', tempo: '80', reach: 10, resistance: 6, duration: 45 },
+    { id: 'stretch-2', hotspot: 'Stretch', tempo: '80', reach: 10, resistance: 2, duration: 45 },
+    { id: 'strength-1', hotspot: 'Strength', tempo: '100-120', reach: 20, resistance: 11, duration: 50 },
+    { id: 'cooldown-1', hotspot: 'Cooldown', tempo: '60', reach: 20, resistance: 1, duration: 90 }
+]
--- src/index.js
+++ src/index.js
@@ -1,17 +1,109 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { timelineArray, classDuration } from './hotspotsData';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// a little function to help us with reordering the result
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return result;
+};
+
+// const grid = classDuration;
+const grid = 8;
+
+const resistance = timelineArray.map(item => {
+  return item.resistance
+});
+const duration = timelineArray.map(item => {
+  return item.duration
+})
+console.log(resistance, duration)
+
+const getItemStyle = (isDragging, draggableStyle, item) => ({
+  userSelect: 'none',
+  padding: grid * 2,
+  margin: `0 ${grid}px 0 0`,
+  background: isDragging ? 'lightgreen' : 'grey',
+  // background: (resistance > 5) ? 'orange' : 'green',
+  height: `${resistance}px`,
+  width: `${duration}px`,
+  color: 'white',
+  ...draggableStyle,
+});
+
+const getListStyle = isDraggingOver => ({
+  background: isDraggingOver ? 'lightblue' : 'lightgrey',
+  display: 'flex',
+  padding: grid,
+  overflow: 'auto',
+});
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: timelineArray,
+    };
+    this.onDragEnd = this.onDragEnd.bind(this);
+  }
+
+  onDragEnd(result) {
+    // dropped outside the list
+    if (!result.destination) {
+      return;
+    }
+
+    const items = reorder(
+      this.state.items,
+      result.source.index,
+      result.destination.index
+    );
+
+    this.setState({
+      items,
+    });
+  }
+
+  render() {
+    return (
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <Droppable droppableId="droppable" direction="horizontal">
+          {(provided, snapshot) => (
+            <div
+              ref={provided.innerRef}
+              style={getListStyle(snapshot.isDraggingOver)}
+              {...provided.droppableProps}
+            >
+              {this.state.items.map((item, index) => (
+                <Draggable key={item.id} draggableId={item.id} index={index}>
+                  {(provided, snapshot) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      style={getItemStyle(
+                        snapshot.isDragging,
+                        provided.draggableProps.style
+                      )}
+                    >
+                      {item.id} <br />
+                      {item.duration} seconds
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    );
+  }
+}
+
+// Put the thing into the DOM!
+ReactDOM.render(<App />, document.getElementById('root'));
